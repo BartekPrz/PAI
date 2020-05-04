076f75c2effddff9890be78e88674a5ef202b434
Bartosz Przybyl <bartolomeo369@gmail.com>
2020-05-04 23:47:57 +0200

    Add Zad9 ASP.NET MVC solution

---
+++ Zadanie9/SongsController(Przed usunięciem).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class SongsController : Controller
+    {
+        // GET: Songs
+        public ActionResult Index()
+        {
+            Song song = new Song();
+            song.Name = "Shape of You";
+            song.Artist = "Ed Sheeran";
+            song.Genre = "POP";
+            song.Id = 1;
+            return View(song);
+        }
+
+        //GET id squared
+        public ActionResult Square(int id)
+        {
+            return Content(Math.Pow(id, 2).ToString());
+        }
+    }
+}
